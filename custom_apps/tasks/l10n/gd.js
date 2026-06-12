cf9ce62a54b6b834cff5ac2a545680781d114bd4
Tareq Kirresh <kirreshtareq@gmail.com>
2026-06-13 01:45:48 +0300

    [hou[housekeeping-0000] update tasks and previewgenerator

---
+++ custom_apps/tasks/l10n/gd.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "tasks",
+    {
+    "Today" : "An-diugh",
+    "Current" : "Làithreach",
+    "Completed" : "Coileanta",
+    "Edit" : "Deasaich",
+    "Delete" : "Sguab às",
+    "Unshare" : "Na co-roinn tuilleadh",
+    "Empty trash bin" : "Falamhaich an sgudal",
+    "Name" : "Ainm",
+    "Deleted" : "Chaidh a sguabadh às",
+    "Cancel" : "Sguir dheth",
+    "Email" : "Post-d",
+    "Close" : "Dùin",
+    "Tags" : "Tagaichean",
+    "Details" : "Mion-fhiosrachadh",
+    "Notes" : "Nòtaichean"
+},
+"nplurals=4; plural=(n==1 || n==11) ? 0 : (n==2 || n==12) ? 1 : (n > 2 && n < 20) ? 2 : 3;");
---
+++ custom_apps/tasks/l10n/sr@latin.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "tasks",
+    {
+    "Today" : "Today",
+    "All" : "Sve",
+    "Current" : "Tekuće",
+    "Completed" : "Završeno",
+    "Can edit" : "Can edit",
+    "Edit" : "Uredi",
+    "Delete" : "Delete",
+    "Unshare" : "Ukini deljenje",
+    "Empty trash bin" : "Isprazni kantu",
+    "Name" : "Ime",
+    "Deleted" : "Obrisano",
+    "Delete permanently" : "Obriši zauvek",
+    "Cancel" : "Cancel",
+    "Close" : "Zatvori",
+    "Due date" : "Rok",
+    "Tags" : "Oznake",
+    "Details" : "Detalji",
+    "Notes" : "Beleške"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
